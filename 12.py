3a4a8b947318c263f59b2583579ff995269dcdea
bksahan5569@gmail.com <bksahan5569@gmail.com>
2023-08-03 12:48:24 +0530

    Mine_first

--- 12.py
+++ 12.py
@@ -6,8 +6,8 @@ print("Skill test 01")
 print("Skill test 01")
 print("Skill test 01")
 print("Skill test 01")
+print("Skill test 01")httpshttps://github.com/HemanthKR07/skill_test_01/blob/main/new_test.py
 print("Skill test 01")
 print("Skill test 01")
-print("Skill test 01")
-print("Skill test 01")
+print("Skill test 01")kasfhafh
 
