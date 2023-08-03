0641894e221028cbaaeb2f11f7e26a0a20ed7d6e
bksahan5569@gmail.com <bksahan5569@gmail.com>
2023-08-03 12:50:01 +0530

    99

--- 12.py
+++ 12.py
@@ -6,7 +6,7 @@ print("Skill test 01")
 print("Skill test 01")
 print("Skill test 01")
 print("Skill test 01")
-print("Skill test 01")httpshttps://github.com/HemanthKR07/skill_test_01/blob/main/new_test.py
+print("Skill test 01")httpshnttps://github.com/HemanthKR07/skill_test_01/blob/main/new_test.py
 print("Skill test 01")
 print("Skill test 01")
 print("Skill test 01")kasfhafhjhg
